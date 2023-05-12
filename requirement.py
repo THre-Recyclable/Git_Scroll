c8b3aa04abe9f57fba0ec323a23b2daab1fc3070
THre <forigve7145@gmail.com>
2023-05-12 20:42:47 +0900

    updated requirement.py
    added installing gitpython

--- requirement.py
+++ requirement.py
@@ -1,8 +1,14 @@
 import subprocess
 
 pyqt5 = ["pip", "install", "PyQt5"]
+gitpython = ["pip", "install", "GitPython"]
 
 ### install PyQt5 gui package
 print("{}".format(" ".join(pyqt5)), end="\n")
 out = subprocess.check_output(pyqt5, shell = True)
+print(out.decode(), end = "\n\n")
+
+### install GitPython pacakage
+print("{}".format(" ".join(gitpython)), end="\n")
+out = subprocess.check_output(gitpython, shell = True)
 print(out.decode(), end = "\n\n")
